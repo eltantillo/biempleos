75c12fceafecc3c1213c1e6b0d57e4faef0822d7
David Morales <David Morales>
2016-08-19 09:03:59 -0600

    UPDATE

--- protected/config/database.php
+++ protected/config/database.php
@@ -7,6 +7,6 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=biempleos',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => 'asd',
+	'password' => '123',
 	'charset' => 'utf8',
 );
